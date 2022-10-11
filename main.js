9cba8a31e315c1a6974bd6796d9a137a966f92d2
Fede-Arevalo <arevalo.feder@gmail.com>
2022-10-11 14:25:26 +0200

    actualización de objetos

--- main.js
+++ main.js
@@ -9,152 +9,107 @@ let currentQuestionIndex;
 let nota = 0;
 const questions = [
   {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
+    category: "Entertainment: Television",
+    type: "multiple",
+    difficulty: "medium",
+    question: "From what show is the character &quot;James Doakes&quot;? ",
+    correct_answer: "Dexter",
+    incorrect_answers: [
+      "Marvels Daredevil",
+      "Boardwalk Empire",
+      "The Walking Dead",
     ],
   },
   {
-    question: "Is web development fun?",
-    answers: [
-      { text: "Kinda", correct: false },
-      { text: "YES!!!", correct: true },
-      { text: "Um no", correct: false },
-      { text: "IDK", correct: false },
+    category: "Entertainment: Film",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which of these movies did Jeff Bridges not star in?",
+    correct_answer: "The Hateful Eight",
+    incorrect_answers: ["Tron: Legacy", "The Giver", "True Grit"],
+  },
+  {
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "What is the full name of the protagonist from the SNES game Clock Tower?",
+    correct_answer: "Jennifer Simpson",
+    incorrect_answers: [
+      "Jennifer Barrows",
+      "Jennifer Cartwright",
+      "Jennifer Maxwell",
     ],
   },
   {
-    question: "What is 4 * 2?",
-    answers: [
-      { text: "6", correct: false },
-      { text: "8", correct: true },
-      { text: "Yes", correct: false },
+    category: "Entertainment: Board Games",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which one of these is not a real game in the Dungeons &amp; Dragons series?",
+    correct_answer: "Extreme Dungeons &amp; Dragons",
+    incorrect_answers: [
+      "Advanced Dungeons &amp; Dragons",
+      "Dungeons &amp; Dragons 3.5th edition",
+      "Advanced Dungeons &amp; Dragons 2nd edition",
     ],
+  },
+  {
+    category: "Geography",
+    type: "multiple",
+    difficulty: "medium",
+    question: "How tall is One World Trade Center in New York City?",
+    correct_answer: "1,776 ft",
+    incorrect_answers: ["1,888 ft", "1,225 ft", "1,960 ft"],
+  },
+  {
+    category: "History",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "What was the code name for the Allied invasion of Southern France on August 15th, 1944?",
+    correct_answer: "Operation Dragoon",
+    incorrect_answers: [
+      "Operation Overlord",
+      "Operation Market Garden",
+      "Operation Torch",
+    ],
+  },
+  {
+    category: "Animals",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The Killer Whale is considered a type of dolphin.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Japanese Anime & Manga",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "In the &quot;To Love-Ru&quot; series, Golden Darkness is sent to kill Lala Deviluke.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Sports",
+    type: "multiple",
+    difficulty: "medium",
+    question: "Which car manufacturer won the 2017 24 Hours of Le Mans?",
+    correct_answer: "Porsche",
+    incorrect_answers: ["Toyota", "Audi", "Chevrolet"],
+  },
+  {
+    category: "Entertainment: Video Games",
+    type: "multiple",
+    difficulty: "hard",
+    question:
+      "&quot;Strangereal&quot; is a fictitious Earth-like world for which game series?",
+    correct_answer: "Ace Combat",
+    incorrect_answers: ["Jet Set Radio", "Deus Ex", "Crimson Skies"],
   },
 ];
-
-// {
-//   "response_code": 0,
-//   "results": [
-//     {
-//       "category": "Entertainment: Television",
-//       "type": "multiple",
-//       "difficulty": "medium",
-//       "question": "From what show is the character &quot;James Doakes&quot;? ",
-//       "correct_answer": "Dexter",
-//       "incorrect_answers": [
-//         "Marvel&#039;s Daredevil",
-//         "Boardwalk Empire",
-//         "The Walking Dead"
-//       ]
-//     },
-//     {
-//       "category": "Entertainment: Film",
-//       "type": "multiple",
-//       "difficulty": "easy",
-//       "question": "Which of these movies did Jeff Bridges not star in?",
-//       "correct_answer": "The Hateful Eight",
-//       "incorrect_answers": [
-//         "Tron: Legacy",
-//         "The Giver",
-//         "True Grit"
-//       ]
-//     },
-//     {
-//       "category": "Entertainment: Video Games",
-//       "type": "multiple",
-//       "difficulty": "medium",
-//       "question": "What is the full name of the protagonist from the SNES game Clock Tower?",
-//       "correct_answer": "Jennifer Simpson",
-//       "incorrect_answers": [
-//         "Jennifer Barrows",
-//         "Jennifer Cartwright",
-//         "Jennifer Maxwell"
-//       ]
-//     },
-//     {
-//       "category": "Entertainment: Board Games",
-//       "type": "multiple",
-//       "difficulty": "easy",
-//       "question": "Which one of these is not a real game in the Dungeons &amp; Dragons series?",
-//       "correct_answer": "Extreme Dungeons &amp; Dragons",
-//       "incorrect_answers": [
-//         "Advanced Dungeons &amp; Dragons",
-//         "Dungeons &amp; Dragons 3.5th edition",
-//         "Advanced Dungeons &amp; Dragons 2nd edition"
-//       ]
-//     },
-//     {
-//       "category": "Geography",
-//       "type": "multiple",
-//       "difficulty": "medium",
-//       "question": "How tall is One World Trade Center in New York City?",
-//       "correct_answer": "1,776 ft",
-//       "incorrect_answers": [
-//         "1,888 ft",
-//         "1,225 ft",
-//         "1,960 ft"
-//       ]
-//     },
-//     {
-//       "category": "History",
-//       "type": "multiple",
-//       "difficulty": "hard",
-//       "question": "What was the code name for the Allied invasion of Southern France on August 15th, 1944?",
-//       "correct_answer": "Operation Dragoon",
-//       "incorrect_answers": [
-//         "Operation Overlord",
-//         "Operation Market Garden",
-//         "Operation Torch"
-//       ]
-//     },
-//     {
-//       "category": "Animals",
-//       "type": "boolean",
-//       "difficulty": "easy",
-//       "question": "The Killer Whale is considered a type of dolphin.",
-//       "correct_answer": "True",
-//       "incorrect_answers": [
-//         "False"
-//       ]
-//     },
-//     {
-//       "category": "Entertainment: Japanese Anime & Manga",
-//       "type": "boolean",
-//       "difficulty": "easy",
-//       "question": "In the &quot;To Love-Ru&quot; series, Golden Darkness is sent to kill Lala Deviluke.",
-//       "correct_answer": "False",
-//       "incorrect_answers": [
-//         "True"
-//       ]
-//     },
-//     {
-//       "category": "Sports",
-//       "type": "multiple",
-//       "difficulty": "medium",
-//       "question": "Which car manufacturer won the 2017 24 Hours of Le Mans?",
-//       "correct_answer": "Porsche",
-//       "incorrect_answers": [
-//         "Toyota",
-//         "Audi",
-//         "Chevrolet"
-//       ]
-//     },
-//     {
-//       "category": "Entertainment: Video Games",
-//       "type": "multiple",
-//       "difficulty": "hard",
-//       "question": "&quot;Strangereal&quot; is a fictitious Earth-like world for which game series?",
-//       "correct_answer": "Ace Combat",
-//       "incorrect_answers": [
-//         "Jet Set Radio",
-//         "Deus Ex",
-//         "Crimson Skies"
-//       ]
-//     }
-//   ]
-// }
 
 function startGame() {
   startButton.classList.add("hide");
@@ -179,18 +134,18 @@ function showQuestion(question) {
       if (button.dataset.correct == "true") {
         nota++;
         notaElement.innerHTML = "Tu puntuación: " + nota;
-        console.log(nota);
       } else {
         if (nota != 0) {
           nota = nota - 0.5;
           notaElement.innerHTML = "Tu puntuación: " + nota;
-        }else{
-            notaElement.innerHTML = "Tu puntuación: " + nota;
+        } else {
+          notaElement.innerHTML = "Tu puntuación: " + nota;
         }
       }
 
       selectAnswer();
     });
+
     answerButtonsElement.appendChild(button);
   });
 }
